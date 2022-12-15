ab1cb62b5c869ff8c87ea35b6e852423c98f46d0
thiagobmi <thiagomorais.aluno@unipampa.edu.br>
2022-12-15 03:48:10 -0300

    adicionei funções

--- celso/function.c
+++ celso/function.c
@@ -47,11 +47,64 @@ float vetorSoma(float*vet1,float*vet2, int tamanho){
     for(int i=0;i<tamanho;i++)somatorio += vet1[i] + vet2[i];
     return somatorio;
 };
+
+float *diferencaVetores(float *vet1, float *vet2, int tamanho){
+
+    float *resultado=vetorCria(tamanho);
+
+    for(int i=0;i<tamanho;i++)resultado[i]=vet1[i]-vet2[i];
+    return resultado;
+
+}   
+
+float multiplicaVetores(float *vet1,float *vet2, int tamanho){ 
+
+    int resultado=0;
+    for(int i=0;i<tamanho;i++)resultado+=vet1[i]*vet2[i];
+    return resultado;
+}
+
+float *encontraVetor_W_Individual(float *vet1,float *vet2,int tamanho){
+
+    float *vetorW=vetorCria(tamanho);
+    float coeficiente=encontraCoeficiente(vet1,vet2,tamanho);
+
+
+    for(int i=0;i<tamanho;i++)vetorW[i]=vet1[i]-(vet2[i]*coeficiente);
+
+}
+
+float encontraCoeficiente(float *vet1, float *vet2, int tamanho){
+
+    float coeficienteA=0;
+    coeficienteA=multiplicaVetores(vet1,vet2,tamanho)/multiplicaVetores(vet2,vet2,tamanho);
+    return coeficienteA;
+}
+
+//Wn=Vn - Somatorio de W1a1 a Wnan-1
+
+float encontraWn(float **vetores,int tamanho,int n){
+
+    vetores=malloc(sizeof(float)*n);
+    for(int i=0;i<n;i++)vetores[i]=vetorCria(tamanho);
+
+    float **vetoresRefined=malloc(sizeof(float)*n);
+    for(int i=0;i<n;i++)vetoresRefined[i]=vetorCria(tamanho);
+
+    for(int i=0;i<tamanho;i)vetoresRefined[1][i]=vetores[1][i];
+    
+    
+}
+
+
+
 float vetorModulo(float*vet1, int tamanho){
     float acumulador=0;
     for (int i = 0; i < tamanho; i++) acumulador += pow(vet1[i],2);
     return sqrt(acumulador);
 };
+
+
 //funcoes matrizes
 float **matriz2dCria(int linha,int coluna){};
 float **matriz2dPreenche(float**matriz,int linha,int coluna){};
